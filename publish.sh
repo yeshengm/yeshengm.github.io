8057baee5a75ae0a0e821f52d7b77c4ed42dcf30
ysma <kimi.ysma@gmail.com>
2018-12-17 04:10:54 -0500

    src updated on Mon Dec 17 04:10:54 EST 2018

--- publish.sh
+++ publish.sh
@@ -4,13 +4,13 @@ git checkout dev
 git add --all & git commit -am "src updated on `date`"
 stack exec site clean
 stack exec site build
-git push origin dev
+git push -f origin dev
 # commit master
 git fetch --all
 git checkout -b master --track origin/master
 cp -a _site/. .
 git add --all & git commit -am "published on `date`"
-git push origin master
+git push -f origin master
 # back to dev and delete master
 git checkout dev
 git branch -D master

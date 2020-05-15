441d98bc561e7df55f3267aa6dd775f5e37c3011
Yesheng Ma <kimi.ysma@gmail.com>
2020-05-15 00:40:16 -0700

    published on Fri May 15 00:40:16 PDT 2020

--- publish.sh
+++ publish.sh
@@ -12,7 +12,7 @@ git branch -D master
 git checkout -b master --track origin/master
 cp -a _site/. .
 git add --all & git commit -am "published on `date`"
-git push -f origin/master
+git push -f origin master
 # back to dev and delete master
 git checkout -f dev
 git clean -fdx

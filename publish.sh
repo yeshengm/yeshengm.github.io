a777d618c4cfd176c85e29f2bab7a841d8933a9a
Yesheng Ma <kimi.ysma@gmail.com>
2020-05-15 00:32:11 -0700

    published on Fri May 15 00:32:11 PDT 2020

--- publish.sh
+++ publish.sh
@@ -12,7 +12,7 @@ stack exec hakyll clean
 stack exec hakyll build
 cp -a _site/. .
 git add --all & git commit -am "published on `date`"
-git push -f origin master
+git push -f origin/master
 # back to dev and delete master
 git checkout -f dev
 git branch -D master

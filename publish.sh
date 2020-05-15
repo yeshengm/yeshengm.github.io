29507b0464f84d87b0f0c910362dab31934e7c67
Yesheng Ma <kimi.ysma@gmail.com>
2020-05-15 00:34:57 -0700

    cleanup

--- publish.sh
+++ publish.sh
@@ -3,19 +3,18 @@ set -x
 # commit dev
 git checkout dev
 git push -f origin dev
+stack build
+stack exec hakyll clean
+stack exec hakyll build
 # commit master
 git fetch --all
 git branch -D master
 git checkout -b master --track origin/master
-stack build
-stack exec hakyll clean
-stack exec hakyll build
 cp -a _site/. .
 git add --all & git commit -am "published on `date`"
 git push -f origin/master
 # back to dev and delete master
 git checkout -f dev
-git branch -D master
 git clean -fdx
 # sync to andrew
 # rsync -r -a -vv --delete _site/ yeshengm@linux.andrew.cmu.edu:~/www/
